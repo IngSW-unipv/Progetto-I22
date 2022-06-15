8ee66bf584b05c77267e97ae9aa996a9e5fb74f4
GigioMagno <97558303+GigioMagno@users.noreply.github.com>
2022-06-15 11:03:37 +0200

    monitors modifier

--- Restaurant/src/it/unipv/po/cosi/restaurant/model/menuModel/servingModel/Serving.java
+++ Restaurant/src/it/unipv/po/cosi/restaurant/model/menuModel/servingModel/Serving.java
@@ -237,7 +237,7 @@ public class Serving implements Comparable<Serving> {
 		
 		for (Modifier modifier : modifiers) {
 			
-			rslt = rslt + " + " + modifier.getName();
+			rslt = rslt + "\n         " + " + " + modifier.getName();
 			
 		}
 		
